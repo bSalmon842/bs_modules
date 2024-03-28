b5acdf676fbae0fb35c6345cedc59c6ea4a0c675
Brock Salmon <bg100197@gmail.com>
2024-03-29 00:42:41 +1000

    29MAR2024: Re-add ImGui Win32 WndProc implementation

--- ImGui_1_9/src/imgui-1.90.4/backends/imgui_impl_win32.cpp
+++ ImGui_1_9/src/imgui-1.90.4/backends/imgui_impl_win32.cpp
@@ -22,10 +22,6 @@
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
-#include <windows.h>
-#include <windowsx.h> // GET_X_LPARAM(), GET_Y_LPARAM()
-#include <tchar.h>
-#include <dwmapi.h>
 
 // Configuration flags to add in your imconfig.h file:
 //#define IMGUI_IMPL_WIN32_DISABLE_GAMEPAD              // Disable gamepad support. This was meaningful before <1.81 but we now load XInput dynamically so the option is now less relevant.
